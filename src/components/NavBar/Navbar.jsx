cd0914bd8d1c3c3bbafe5d6b18cda2504c8e1c6b
danazou <dana.sou@gmail.com>
2023-03-17 18:30:17 -0700

    rendered react icon fot Navbar

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './navbar.css'
+import {MdOutlineTravelExplore} from 'react-icons/md'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <section className="navBarSection">
+        <header className="header flex">
+
+            <div className="logoDiv">
+                <a href="#" className="logo">
+                <h1> <MdOutlineTravelExplore
+                className="icon"/> Travel.</h1>
+                </a>
+            </div>
+
+        </header>
+    </section>
   )
 }
 
