6553324ec0fbc127c77e12f90b1fbd408ab2f820
danazou <dana.sou@gmail.com>
2023-03-17 17:52:18 -0700

    rendering components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import './app.css'
+import Footer from './components/Footer/Footer'
+import Main from './components/Main/Main'
+import Home from './components/Home/Home'
+import Navbar from './components/Navbar/Navbar'
 
 
 const App = () => {
   return (
-    <div>Working</div>
+    <>
+    <Navbar/>
+    {/* <Home/>
+    <Main/>
+    <Footer/> */}
+    </>
+
   )
 }
 
