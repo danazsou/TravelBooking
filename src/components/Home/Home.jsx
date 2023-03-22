532ceacf108dce84d2cb83e8137b97f9e5ed52b6
danazou <dana.sou@gmail.com>
2023-03-21 23:47:50 -0700

    added imaged overlay, started Home page components - beginning styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import'./home.css'
+import video from '../../Assets/plane-video.mp4'
 
 const Home = () => {
   return (
-    <div>home</div>
+    <section className='home'>
+      <div className="overlay"></div>
+      <video src={video} muted autoPlay loop type ="video/mp4"></video>
+
+      <div className="homeContent container">
+        <div className="textDiv"></div>
+          <span className="smallText">Our Packages</span>
+          <h1 className="homeTitle">Search your Vacation</h1>
+
+      </div>
+
+    </section>
   )
 }
 
