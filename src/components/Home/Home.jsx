5e36a25c2fe679f541fedcf76a6198e7edbf5f3d
danazou <dana.sou@gmail.com>
2023-03-22 14:09:40 -0700

    add location  icon and search imput for destination

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,36 @@
 import React from 'react'
 import'./home.css'
 import video from '../../Assets/plane-video.mp4'
+import{GrLocation} from 'react-icons/gr'
 
 const Home = () => {
   return (
-    <section className='home'>
-      <div className="overlay"></div>
-      <video src={video} muted autoPlay loop type ="video/mp4"></video>
+    <section className= "home">
+      <div className= "overlay"></div>
+        <video src = {video} muted autoPlay loop type = "video/mp4"></video>
 
-      <div className="homeContent container">
-        <div className="textDiv"></div>
-          <span className="smallText">Our Packages</span>
-          <h1 className="homeTitle">Search your Vacation</h1>
+        <div className= "homeContent container">
+          <div className= "textDiv">
+           
+            <span className= "smallText">
+              Our Packages
+            </span>
+            
+              <h1 className= "homeTitle">
+                Search your Vacation 
+              </h1>
+          </div>
 
-      </div>
-
+          <div className="cardDiv grid">
+            <div className="destinationInput"></div>
+              <label htmlFor= "city">Search your destination:</label>
+              <div className="input flex"> 
+                <input type="text" placeholder= "Enter name here..." />
+                <GrLocation className= "icon"/>
+              </div>
+          </div>
+        </div>
+  
     </section>
   )
 }
