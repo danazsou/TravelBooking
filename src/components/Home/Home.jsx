decaf7752bcf26b3cfe352ca5ab0ab398617b450
danazou <dana.sou@gmail.com>
2023-03-22 17:40:09 -0700

    rendered input fot destination, date, and price range

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,11 +3,13 @@ import'./home.css'
 import video from '../../Assets/plane-video.mp4'
 import{GrLocation} from 'react-icons/gr'
 
+
 const Home = () => {
   return (
     <section className= "home">
       <div className= "overlay"></div>
-        <video src = {video} muted autoPlay loop type = "video/mp4"></video>
+        <video src = {video} muted autoPlay loop 
+        type = "video/mp4"></video>
 
         <div className= "homeContent container">
           <div className= "textDiv">
@@ -22,14 +24,37 @@ const Home = () => {
           </div>
 
           <div className="cardDiv grid">
-            <div className="destinationInput"></div>
-              <label htmlFor= "city">Search your destination:</label>
+            <div className="destinationInput">
+              <label htmlFor= "city">Search your 
+              destination:</label>
               <div className="input flex"> 
                 <input type="text" placeholder= "Enter name here..." />
                 <GrLocation className= "icon"/>
               </div>
+              </div>
+
+            <div className="dateInput">
+              <label htmlFor= "date">Select your
+              date:</label>
+              <div className="input flex"> 
+                <input type="date"/>
+              </div>
+              </div>
+
+              <div className="priceInput">
+                <div className="label_total flex">
+                  <label htmlFor= "price">Max price:
+                  </label>
+                  <h3 className="total">$5000</h3>
+                  </div>
+                  <div className="input flex">
+                    <input type= "range" max="5000"
+                    min="1000" />
+                  </div>
+                </div>
+              </div>
           </div>
-        </div>
+        
   
     </section>
   )
