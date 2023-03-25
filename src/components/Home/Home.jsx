b39fdddb17fb51c7a0c6fd0f5b1ddd4442882d5d
danazou <dana.sou@gmail.com>
2023-03-25 15:59:42 -0700

    rendered and styled footer icons

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,11 @@ import'./home.css'
 import video from '../../Assets/plane-video.mp4'
 import{GrLocation} from 'react-icons/gr'
 import{HiFilter} from 'react-icons/hi'
+import{FiFacebook} from 'react-icons/fi'
+import{AiOutlineInstagram} from 'react-icons/ai'
+import{SiTripadvisor} from 'react-icons/si'
+import{BsListTask} from 'react-icons/bs'
+import{TbApps} from 'react-icons/tb'
 
 
 const Home = () => {
@@ -58,7 +63,21 @@ const Home = () => {
 
                 <div className="searchOptions flex">
                   <HiFilter className="icon"/>
+                  <span>MORE FILTERS</span>
                 </div>
+              </div>
+
+              <div className="homeFooterIcons flex">
+                <div className="rightIcons">
+                <FiFacebook className="icon"/>
+                <AiOutlineInstagram className="icon"/>
+                <SiTripadvisor className="icon"/>
+                </div>
+
+                <div className="leftIcons">
+                <BsListTask className="icon"/>
+                <TbApps className="icon"/>
+                  </div>
               </div>
          </div>
       
