89e8f0f2e845feda6e215e3fe0bff5a2e995fb25
danazou <dana.sou@gmail.com>
2023-03-26 22:26:06 -0700

    styled fees and grade .. still can't get css to catch on continent and icon

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -131,18 +131,21 @@ const Main = () => {
 
                   <div className="cardInfo">
                     <h4 className="destTitle">{destTitle}</h4>
-                    <span className="continent flex"></span>
-                    <HiOutlineLocationMarker className='icon'/>
-                    <span className="name">{location}</span>
-
+                    <span className="continent flex">
+                      <HiOutlineLocationMarker className='icon'/>
+                      <span className="name">{location}</span>  
+                      </span>
+                  
+                    
                     <div className="fees flex">
                       <div className="grade">
                         <span>{grade}<small>+1</small></span>
-                      </div>
+                     </div>
                       <div className="price">
                         <h5>{fees}</h5>
                       </div>
                     </div>
+
                     <div className="desc">
                       <p>{description}</p>
                     </div>
@@ -150,6 +153,7 @@ const Main = () => {
                     <button className='btn flex'>
                       DETAILS <HiOutlineClipboardCheck className='icon'/>
                     </button>
+                    
                   </div>
                 </div>
               )
