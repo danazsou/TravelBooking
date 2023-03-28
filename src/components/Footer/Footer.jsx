2cbc25c163c07c2d5df3542f2335309b2c9dfdf1
danazou <dana.sou@gmail.com>
2023-03-27 23:00:49 -0700

    added footer icon for email and submit btn

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import'./footer.css'
 import video2 from '../../Assets/video2.mp4'
+import {FiSend} from 'react-icons/fi'
 
 const Footer = () => {
   return (
@@ -10,9 +11,21 @@ const Footer = () => {
       </div>
 
       <div className="secContent container">
-
+        <div className="contactDiv flex">
+          <div className="text">
+            <small>KEEP IN TOUCH</small>
+            <h2>Travel with us</h2>
+          </div>
+          
+          <div className="inputDiv flex">
+            <input type="text" placeholder='Enter Email Address'/>
+            <button className='btn flex' type='submit'>
+              SEND <FiSend className="icon"/>
+            </button>
+          </div>
+        </div>
       </div>
-      
+
     </section>
   )
 }
